d40d067d14355351fc5bd7c7973bc535f4521252
Gabr1el20 <6327@holbertonstudents.com>
2023-09-26 07:35:38 -0700

    Task 1 y 2 (99%) hecha

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  const amenityArr = {};
+  $('input:checkbox').click(function() {
+    if ($(this).is(':checked')) {
+      console.log('Checkbox marcado!')
+      amenityArr[$(this).attr('data-id')] = $(this).attr('data-name');
+    }
+    else {
+      console.log('Checkbox desmarcado')
+      delete amenityArr[$(this).attr('data-id')];
+    }
+    $('.amenities h4').text(Object.values(amenityArr).join(', '));
+  })
+})
